c4ea8f320238ed68cf0436fc656971e7c61ae999
Sujin <cy_20200202@163.com>
2022-01-10 17:20:57 -0500

    96 152 300 673 931 1036 1567

--- lc_Python/lc1_99.py
+++ lc_Python/lc1_99.py
@@ -1226,3 +1226,11 @@ class Solution:
 
 
 # 96 - Unique Binary Search Trees - MEDIUM
+class Solution(object):
+    def numTrees(self, n):
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 1
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j - 1] * dp[i - j]
+        return dp[n]
