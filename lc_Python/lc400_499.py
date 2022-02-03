123a007bd23297d585c56bbbf6416da8b08c217e
Sujin <cy_20200202@163.com>
2022-02-02 23:40:11 -0500

    415

--- lc_Python/lc400_499.py
+++ lc_Python/lc400_499.py
@@ -96,6 +96,21 @@ class Solution:
         return ans
 
 
+# 415 - Add Strings - EASY
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        ans = ""
+        i, j, carry = len(num1) - 1, len(num2) - 1, 0
+        while i >= 0 or j >= 0 or carry != 0:
+            n1 = int(num1[i]) if i >= 0 else 0
+            n2 = int(num2[j]) if j >= 0 else 0
+            tmp = n1 + n2 + carry
+            carry = tmp // 10
+            ans = str(tmp % 10) + ans
+            i, j = i - 1, j - 1
+        return ans
+
+
 # 419 - Battleships in a Board - MEDIUM
 class Solution:
     def countBattleships(self, board):
