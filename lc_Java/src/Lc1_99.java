ceb6b322fb84941ee78f3a4b7f7c08433143fe2d
Sujin <cy_20200202@163.com>
2022-02-04 00:14:31 -0500

    IntelliJ

--- lc_Java/src/Lc1_99.java
+++ lc_Java/src/Lc1_99.java
@@ -1,5 +1,3 @@
-package lc_Java;
-
 //List / Array
 import java.util.Arrays;
 import java.util.ArrayList;
--- lc_Java/src/Lc800_899.java
+++ lc_Java/src/Lc800_899.java
@@ -1,5 +1,3 @@
-package lc_Java;
-
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
