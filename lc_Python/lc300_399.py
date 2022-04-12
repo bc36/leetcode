35d842978028d792880f6c5373e7a6c9e3e8da2c
Sujin <cy_20200202@163.com>
2022-04-12 12:38:47 -0400

    380

--- lc_Python/lc300_399.py
+++ lc_Python/lc300_399.py
@@ -1259,6 +1259,34 @@ class Solution:
         return dp[-1]
 
 
+# 380 - Insert Delete GetRandom O(1) - MEDIUM
+class RandomizedSet:
+    def __init__(self):
+        self.arr = []
+        self.v2idx = {}
+
+    def insert(self, val: int) -> bool:
+        if val in self.v2idx:
+            return False
+        self.v2idx[val] = len(self.arr)
+        self.arr.append(val)
+        return True
+
+    # replace the element to be deleted with the last element, then pop the last one
+    def remove(self, val: int) -> bool:
+        if val not in self.v2idx:
+            return False
+        i = self.v2idx[val]
+        self.v2idx[self.arr[-1]] = i
+        self.arr[i] = self.arr[-1]
+        self.arr.pop()
+        del self.v2idx[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.arr)
+
+
 # 382 - Linked List Random Node - MEDIUM
 class Solution:
     def __init__(self, head: Optional[ListNode]):
