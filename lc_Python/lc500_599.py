4fb6ac4a4efc1351f6ea8d37c912b1fa20cc9f0b
Sujin <cy_20200202@163.com>
2022-04-22 14:37:30 -0400

    587

--- lc_Python/lc500_599.py
+++ lc_Python/lc500_599.py
@@ -871,6 +871,47 @@ class Solution:
         return len1 + len2 - dp[-1][-1] * 2
 
 
+# 587 - Erect the Fence - HARD
+# Jarvis Algorithm                    - O(n ^ 2) / O(n)
+# Graham Scan                         - O(nlogn) / O(n)
+# Monotone Chain / Andrew Algorithm   - O(nlogn) / O(n)
+class Solution:
+    def outerTrees(self, points: List[List[int]]) -> List[List[int]]:
+        """Computes the convex hull of a set of 2D points.
+        Input: an iterable sequence of (x, y) pairs representing the points.
+        Output: a list of vertices of the convex hull in counter-clockwise order,
+          starting from the vertex with the lexicographically smallest coordinates.
+        Implements Andrew's monotone chain algorithm. O(n log n) complexity.
+        """
+        points = sorted(map(tuple, points), key=lambda p: (p[0], p[1]))
+
+        # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
+        # Returns a positive value, if OAB makes a counter-clockwise turn,
+        # negative for clockwise turn, and zero if the points are collinear.
+        def sign(o, a, b):
+            return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] -
+                                                                    o[0])
+
+        # Build lower hull
+        lower = []
+        for p in points:
+            while len(lower) >= 2 and sign(lower[-2], lower[-1], p) < 0:
+                lower.pop()
+            lower.append(p)
+
+        # Build upper hull
+        upper = []
+        for p in reversed(points):
+            while len(upper) >= 2 and sign(upper[-2], upper[-1], p) < 0:
+                upper.pop()
+            upper.append(p)
+
+        # Concatenation of the lower and upper hulls gives the convex hull.
+        # Last point of each list is omitted because it is repeated at the
+        # beginning of the other list.
+        return list(set(lower + upper))
+
+
 # 589 - N-ary Tree Preorder Traversal - EASY
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
