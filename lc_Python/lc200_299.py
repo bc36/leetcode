a54f55d84e813c40e53c45b78ad4193b24ed3e23
sujin <cy_20200202@163.com>
2022-06-24 14:50:22 -0400

    256

--- lc_Python/lc200_299.py
+++ lc_Python/lc200_299.py
@@ -1218,6 +1218,16 @@ class Solution:
         return True
 
 
+# 256 - Paint House - MEDIUM - PREMIUM
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        for i in range(1, len(costs)):
+            costs[i][0] += min(costs[i - 1][1], costs[i - 1][2])
+            costs[i][1] += min(costs[i - 1][0], costs[i - 1][2])
+            costs[i][2] += min(costs[i - 1][0], costs[i - 1][1])
+        return min(costs[-1])
+
+
 # 258 - Add Digits - EASY
 class Solution:
     def addDigits(self, num: int) -> int:
