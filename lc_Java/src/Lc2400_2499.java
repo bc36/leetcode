c967110d3cd19a9b4c058e053abb85674172f07e
Trevor <56862963+bcvi@users.noreply.github.com>
2023-04-04 23:55:00 -0400

    2427

--- lc_Java/src/Lc2400_2499.java
+++ lc_Java/src/Lc2400_2499.java
@@ -71,6 +71,18 @@ public class Lc2400_2499 {
         }
     }
 
+    // 2427. Number of Common Factors - EASY
+    class Solution2427a {
+        public int commonFactors(int a, int b) {
+            int ans = 0, n = Math.min(a, b);
+            for (int i = 1; i <= n; ++i) {
+                if (a % i == 0 && b % i == 0)
+                    ++ans;
+            }
+            return ans;
+        }
+    }
+
     // 2488. Count Subarrays With Median K - HARD
     class Solution2488a {
         // 12ms
