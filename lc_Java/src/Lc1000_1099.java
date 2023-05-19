0a790204b533a43863f04f9bdff79f3c4c871037
Trevor <56862963+bcvi@users.noreply.github.com>
2023-05-18 23:58:20 -0400

    1073

--- lc_Java/src/Lc1000_1099.java
+++ lc_Java/src/Lc1000_1099.java
@@ -678,6 +678,33 @@ public class Lc1000_1099 {
         }
     }
 
+    // 1073. Adding Two Negabinary Numbers - MEDIUM
+    class Solution1073a {
+        public int[] addNegabinary(int[] arr1, int[] arr2) {
+            int i = arr1.length - 1, j = arr2.length - 1;
+            List<Integer> ans = new ArrayList<>();
+            for (int c = 0; i >= 0 || j >= 0 || c != 0; --i, --j) {
+                int a = i < 0 ? 0 : arr1[i];
+                int b = j < 0 ? 0 : arr2[j];
+                int x = a + b + c;
+                c = 0;
+                if (x >= 2) {
+                    x -= 2;
+                    c -= 1;
+                } else if (x == -1) {
+                    x = 1;
+                    c += 1;
+                }
+                ans.add(x);
+            }
+            while (ans.size() > 1 && ans.get(ans.size() - 1) == 0) {
+                ans.remove(ans.size() - 1);
+            }
+            Collections.reverse(ans);
+            return ans.stream().mapToInt(x -> x).toArray();
+        }
+    }
+
     // 1092. Shortest Common Supersequence - HARD
     class Solution1092a {
         // 最长公共子序列
--- lc_Python/lc1000_1099.py
+++ lc_Python/lc1000_1099.py
@@ -1044,6 +1044,41 @@ class Solution:
         return max(cnt.values())
 
 
+# 1073 - Adding Two Negabinary Numbers - MEDIUM
+class Solution:
+    def addNegabinary(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        num = sum((-2) ** (len(arr1) - i - 1) for i, v in enumerate(arr1) if v) + sum(
+            (-2) ** (len(arr2) - i - 1) for i, v in enumerate(arr2) if v
+        )
+        ans = []
+        while num:
+            m = num % (-2)  # m = 0 / -1
+            ans.append(-m)
+            num = (num + m) // (-2)
+        return ans[::-1] if ans else [0]
+
+    def addNegabinary(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        i, j = len(arr1) - 1, len(arr2) - 1
+        c = 0  # carry
+        ans = []
+        while i >= 0 or j >= 0 or c:
+            a = 0 if i < 0 else arr1[i]
+            b = 0 if j < 0 else arr2[j]
+            x = a + b + c
+            c = 0
+            if x >= 2:
+                x -= 2
+                c -= 1
+            elif x == -1:
+                x = 1
+                c += 1
+            ans.append(x)
+            i, j = i - 1, j - 1
+        while len(ans) > 1 and ans[-1] == 0:
+            ans.pop()
+        return ans[::-1]
+
+
 # 1078 - Occurrences After Bigram - EASY
 class Solution:
     def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
